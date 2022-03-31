fbdd9717bcd923afb8d241a59637c5f8019891cc
Logan D'Auria <lxd1644@rit.edu>
2022-03-31 03:50:11 -0400

    cloud testing

--- Assets/ObjectSpawner.cs
+++ Assets/ObjectSpawner.cs
@@ -11,33 +11,33 @@ public class ObjectSpawner : MonoBehaviour
     public float speed;
     public float killCoord = -10;
 
-
+    private GameObject[] objects;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        objects = new GameObject[2];
     }
 
     void Spawn()
     {
-        GameObject obj1 = Instantiate(spawnObject, spawnPos1);
-        GameObject obj2 = Instantiate(spawnObject, spawnPos2);
+        //objects[0] = Instantiate(spawnObject, spawnPos1);
+        //objects[1] = Instantiate(spawnObject, spawnPos2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        obj1.transform.position = new Vector3(obj1.transform.position.x, obj1.transform.position.y, obj1.transform.position.z - speed);
-        obj2.transform.position = new Vector3(obj2.transform.position.x, obj2.transform.position.y, obj2.transform.position.z - speed);
+        objects[1].transform.position = new Vector3(objects[1].transform.position.x, objects[1].transform.position.y, objects[1].transform.position.z - speed);
+        objects[0].transform.position = new Vector3(objects[0].transform.position.x, objects[0].transform.position.y, objects[0].transform.position.z - speed);
 
-        if (obj1.transform.position.z < killCoord)
+        if (objects[0].transform.position.z < killCoord)
         {
-            Destroy(obj1);
+            Destroy(objects[1]);
         }
-        if (obj2.transform.position.z < killCoord)
+        if (objects[1].transform.position.z < killCoord)
         {
-            Destroy(obj2);
+            Destroy(objects[0]);
         }
     }
 }
